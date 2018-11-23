92c33bcb4dbfa79277eb5bf1b57ddc80b182ba95
Pavan <swysoft@gmail.com>
2018-11-23 09:22:05 +0530

    Updated catalog.txt

--- cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/game-control/odd/odd.component.ts
+++ cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/game-control/odd/odd.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { element } from 'protractor';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-odd',
   templateUrl: './odd.component.html',
   styleUrls: ['./odd.component.css']
 })
-export class OddComponent implements OnInit {
-  @Input() number: number;
+export class OddComponent implements OnInit, AfterViewInit{
+  @Input('number') number: number;
+
+  @ViewChild('outputPane') outputPane: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.outputPane.nativeElement.innerHTML = this.number;
   }
 
 }
