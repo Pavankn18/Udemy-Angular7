c6b87a34425420fc91e5b787fd1cd73a011a7638
Pavan <swysoft@gmail.com>
2018-12-16 22:32:03 +0530

    Solution - in progress

--- forms-reactive-assignment-start/forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, RequiredValidator, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from 'src/shared/name-validate-directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      project : new FormControl(null, [Validators.required, forbiddenNameValidator(/test/i)]),
+      mail : new FormControl(null, [Validators.required, Validators.email]),
+      status : new FormControl('stable', [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
+
+
 }
