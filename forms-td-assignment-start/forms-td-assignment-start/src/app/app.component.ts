7c4eded83f702fcbb190d1e4df9a8509bea001e5
Pavan <swysoft@gmail.com>
2018-12-16 15:31:04 +0530

    Solved template driven form

--- forms-td-assignment-start/forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild( 'form' ) userForm: NgForm;
+  formText: string;
+  type = 'Basic';
 
+  submit() {
+    console.log( this.userForm );
+    this.formText = this.userForm.toString();
+    this.userForm.reset();
+    this.setDefault();
+  }
+
+  setDefault() {
+    this.userForm.form.patchValue({
+     // mail: 'test@test'//,
+      type: 'Basic'
+      //password: 'password'
+    });
+  }
+
+  ngOnInit(): void {
+    //Below line wont have any effect!!
+    // this.userForm.form.patchValue({
+    //   mail: 'test@test'
+    //   , type: 'Basic'
+    //   , password: 'password'
+    // });
+  }
 }
